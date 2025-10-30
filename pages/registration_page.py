b3aad3bc4d4902bb9930b258a38aee3bc8ea7e12
Alexandra <baulina.alexandra98@gmail.com>
2025-10-30 22:51:49 +0400

    Знакомство с PageObject. Практика работы с Page Object

---
+++ pages/registration_page.py
@@ -0,0 +1,25 @@
+from playwright.sync_api import Page, expect
+from pages.base_page import BasePage
+
+
+class RegistrationPage(BasePage):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        self.username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        self.password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        self.registration_button = page.get_by_test_id('registration-page-registration-button')
+
+    def fill_registration_form(self, email: str, username: str, password: str):  # Метод заполнения формы регистрации
+        self.email_input.fill(email)
+
+        self.username_input.fill(username)
+
+        self.password_input.fill(password)
+
+    def click_registration_button(self): # Метод клика на кнопку регистрации
+        self.registration_button.click()
+
+
+
