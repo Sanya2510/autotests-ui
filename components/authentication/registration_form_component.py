e22572e59b790f288c8f156c807c98d0f5628c86
Alexandra <baulina.alexandra98@gmail.com>
2025-11-07 18:01:48 +0400

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

---
+++ components/authentication/registration_form_component.py
@@ -0,0 +1,30 @@
+from components.base_component import BaseComponent
+from playwright.sync_api import Page, expect
+
+
+class RegistrationFormComponent(BaseComponent):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        self.username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        self.password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+
+    # Метод заполнения формы регистрации
+    def fill(self, email: str, username: str, password: str):
+        self.email_input.fill(email)
+
+        self.username_input.fill(username)
+
+        self.password_input.fill(password)
+
+    # Метод проверки корректности отображения формы и веденных данных
+    def check_visible(self, email, username, password):
+        expect(self.email_input).to_be_visible()
+        expect(self.email_input).to_have_value(email)
+
+        expect(self.username_input).to_be_visible()
+        expect(self.username_input).to_have_value(username)
+
+        expect(self.password_input).to_be_visible()
+        expect(self.password_input).to_have_value(password)
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import Page, expect
+
+from components.authentication.login_form_component import LoginForm
 from pages.base_page import BasePage
 
 
@@ -6,19 +8,11 @@ class LoginPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.email_input = page.get_by_test_id('login-form-email-input').locator('input')
-        self.password_input = page.get_by_test_id('login-form-password-input').locator('input')
+        self.login_form = LoginForm(page)
+
         self.login_button = page.get_by_test_id('login-page-login-button')
         self.registration_link = page.get_by_test_id('login-page-registration-link')
         self.wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
-
-    # Метод для заполнения формы авторизации
-    def fill_login_form(self, email: str, password: str):
-        self.email_input.fill(email)
-        expect(self.email_input).to_have_value(email)
-
-        self.password_input.fill(password)
-        expect(self.password_input).to_have_value(password)
 
     # Метод для нажатия кнопки "Login"
     def click_login_button(self):
@@ -26,7 +20,7 @@ class LoginPage(BasePage):
 
     # Метод для нажатия на ссылку "Registration"
     def click_registration_link(self):
-        self.registration_link
+        self.registration_link.click()
 
     # Метод для проверки отображения алерта с ошибкой
     def check_visible_wrong_email_or_password_alert(self):
