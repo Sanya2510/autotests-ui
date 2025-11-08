b26cd4209bb1d7c830433e02f8460ba36e24a692
Alexandra <baulina.alexandra98@gmail.com>
2025-11-08 15:54:33 +0400

    Практикуемся в реализации PageComp`onent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- components/authentication/login_form_component.py
+++ components/authentication/login_form_component.py
@@ -1,7 +1,7 @@
 from components.base_component import BaseComponent
 from playwright.sync_api import Page, expect
 
-class LoginForm(BaseComponent):
+class LoginFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
