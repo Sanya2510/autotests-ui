699a73394ea13bbfe45d0e880ef7ba7997d59011
Alexandra <baulina.alexandra98@gmail.com>
2025-11-25 22:50:00 +0400

    Добавление видео в Allure отчет

---
+++ tools/playwright/pages.py
@@ -0,0 +1,21 @@
+import allure
+from playwright.sync_api import Playwright, Page
+
+def initialaize_playwright_page(
+        playwright: Playwright,
+        test_name: str,
+        storage_state: str | None = None
+
+) -> Page:  # type: ignore
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state=storage_state, record_video_dir='./videos')
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    page = context.new_page()
+
+    yield page  # type: ignore
+
+    context.tracing.stop(path=f'./tracing/{test_name}.zip')
+    browser.close()
+
+    allure.attach.file(f'./tracing/{test_name}.zip', name='trace', extension='zip')
+    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
