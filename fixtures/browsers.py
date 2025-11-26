27c08388f4e7139705a225b16831ad50bcf2573a
Alexandra <baulina.alexandra98@gmail.com>
2025-11-26 21:18:31 +0400

    Настройки автотестов с Pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -5,7 +5,7 @@ from playwright.sync_api import Page, Playwright
 
 from pages.authentication.registration_page import RegistrationPage
 from tools.playwright.pages import initialaize_playwright_page
-
+from config import settings
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:  # type: ignore
@@ -20,10 +20,14 @@ def initialize_browser_state(playwright: Playwright) -> Page:  # type: ignore
 
     registration_page = RegistrationPage(page=page)
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.registration_form.fill(email='user.name@gmail.com',username='username',password='password')
+    registration_page.registration_form.fill(
+        email=settings.test_user.email,
+        username=settings.test_user.username,
+        password=settings.test_user.password
+    )
     registration_page.click_registration_button()
 
-    context.storage_state(path='browser-state.json')
+    context.storage_state(path=settings.browser_state_file)
 
 
 @pytest.fixture
@@ -31,5 +35,5 @@ def chromium_page_with_state(request: SubRequest, initialize_browser_state, play
     yield from initialaize_playwright_page(  # type: ignore
         playwright,
         test_name=request.node.name,
-        storage_state='browser-state.json'
+        storage_state=settings.browser_state_file
     )
